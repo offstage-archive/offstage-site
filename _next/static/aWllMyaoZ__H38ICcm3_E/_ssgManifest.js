637d8172a9fcc8061f38a52582013e34cfa1522d
Offstage Editor <offstage-editor@proton.me>
2026-07-07 11:17:28 +0900

    Deploy 2026-07-07T02:17Z

---
+++ _next/static/aWllMyaoZ__H38ICcm3_E/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fentities\u002F[slug]","\u002Fja\u002Fentities\u002F[slug]","\u002Fja\u002Fpeople\u002F[slug]","\u002Fpeople\u002F[slug]","\u002Ftl\u002Fentities\u002F[slug]","\u002Ftl\u002Fpeople\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
